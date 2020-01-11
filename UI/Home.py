7682fcb490c56da75c785c2287f242da6e9f184b
AlicanBen <alican.ben.97@gmail.com>
2020-01-11 19:31:43 +0300

    Create

--- UI/Home.py
+++ UI/Home.py
@@ -7,7 +7,7 @@ class Home:
 
     def createWindow(self):
         self.win = QMainWindow()
-        self.win.setGeometry(50, 50, 600, 500)
+        self.win.setGeometry(50, 50, 680, 557)
         self.win.setWindowTitle("Home")
 
     def showWindow(self):
