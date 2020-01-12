a80f98eb6e5021f57b9c6654bdb6cde301ddf5cd
AlicanBen <alican.ben.97@gmail.com>
2020-01-12 20:16:44 +0300

    Commit #1

--- UI/Netdiscover.py
+++ UI/Netdiscover.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QLabel, QPushButton, QVBoxLayout, QLineEdit, QGroupBox, QMainWindow, QWidget
+from PyQt5.QtWidgets import QLabel, QPushButton, QVBoxLayout, QLineEdit, QGroupBox, QMainWindow, QWidget, QComboBox, \
+    QCheckBox, QHBoxLayout, QFileDialog
 
 from Utils.Tools import Tools
 
@@ -10,8 +11,9 @@ class Netdiscover:
     def createWindow(self):
         self.win = QMainWindow()
         self.win.setMinimumWidth(250)
-        self.win.setWindowTitle("Maskprocessor")
+        self.win.setWindowTitle("Netdiscover")
         self.form()
+
         wid = QWidget(self.win)
         self.win.setCentralWidget(wid)
         wid.setLayout(self.vBox)
@@ -23,33 +25,32 @@ class Netdiscover:
     def form(self):
         self.vBox = QVBoxLayout()
 
-        lbl_password = QLabel("Password (with builtin charset)")
-        self.password_edit = QLineEdit()
+        self.range_comboBox=QComboBox()
+        self.range_comboBox.addItems(["Select One","Range","List Of Range File"])
+        self.range_comboBox.setCurrentText("Select One")
+        self.range_comboBox.currentTextChanged.connect(self.comboBoxChange)
+        self.comboBoxGbox=QGroupBox()
+        vBox=QVBoxLayout()
+        self.comboBoxGbox.setVisible(False)
+
+        self.range_label=QLabel("Range (e.g. 1.1.1.1/24)")
+        self.range_edit=QLineEdit()
+        self.fileDialog()
+        self.rangeDialog()
+        vBox.addWidget(self.rangeGBox)
+        vBox.addWidget(self.fileGBox)
+
+        self.comboBoxGbox.setLayout(vBox)
 
         lbl_file_name = QLabel("Output File Name")
         self.file_name_edit = QLineEdit()
 
-        gbox = QGroupBox()
-        g_vBox = QVBoxLayout()
-
-        g_vBox.addWidget(QLabel("Buildin charsets:"))
-        g_vBox.addWidget(QLabel("     ?l = abcdefghijklmnopqrstuvwxyz"))
-        g_vBox.addWidget(QLabel("     ?u = ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
-        g_vBox.addWidget(QLabel("     ?d = 0123456789"))
-        g_vBox.addWidget(QLabel("     ?s =  !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"))
-        g_vBox.addWidget(QLabel("     ?h = 8 bit characters from 0xc0 - 0xff"))
-        g_vBox.addWidget(QLabel("     ?D = 8 bit characters from german alphabet"))
-        g_vBox.addWidget(QLabel("     ?F = 8 bit characters from french alphabet"))
-        g_vBox.addWidget(QLabel("     ?R = 8 bit characters from russian alphabet"))
-        gbox.setLayout(g_vBox)
-
-        self.createButton = QPushButton("Create List")
-
-        self.vBox.addWidget(lbl_password)
-        self.vBox.addWidget(self.password_edit)
+        self.createButton = QPushButton("Run")
+        self.vBox.addWidget(self.range_comboBox)
+        self.vBox.addWidget(self.comboBoxGbox)
         self.vBox.addWidget(lbl_file_name)
         self.vBox.addWidget(self.file_name_edit)
-        self.vBox.addWidget(gbox)
+        self.options()
         self.vBox.addWidget(self.createButton)
 
     def createMenu(self):
@@ -81,6 +82,95 @@ class Netdiscover:
         report.addAction("Show")
         report.addAction("Delete")
         bar.addAction("About Us")
+
+    def options(self):
+        optionsGBox = QGroupBox("Options");
+        vBox = QVBoxLayout()
+
+        self.passive_mode_check = QCheckBox()
+        self.sleep_time_check = QCheckBox()
+        self.count_of_request = QCheckBox()
+
+
+        self.passive_mode_check.setText("Passive Mode")
+
+        self.sleep_time_check.setText("Time To Sleep Between Each ARP Request (default 1)")
+        self.sleep_time_check.toggled.connect(lambda : self.timeCheckHandler(self.sleep_time_check))
+        self.sleep_time_check_gBox=QGroupBox()
+        self.sleep_time_edit=QLineEdit()
+        a= QVBoxLayout()
+        a.addWidget(self.sleep_time_edit)
+        self.sleep_time_check_gBox.setLayout(a)
+        self.sleep_time_check_gBox.setVisible(False)
+        self.sleep_time_check
+
+
+        self.count_of_request.setText("Number Of Times To Send Request")
+        self.count_of_request.toggled.connect(lambda : self.countOfRequestCheckHandler(self.count_of_request))
+        self.count_of_request_gBox=QGroupBox()
+        self.count_of_request_edit=QLineEdit()
+        b = QVBoxLayout()
+        b.addWidget(self.count_of_request_edit)
+        self.count_of_request_gBox.setLayout(b)
+        self.count_of_request_gBox.setVisible(False)
+
+        vBox.addWidget(self.passive_mode_check)
+        vBox.addWidget(self.sleep_time_check)
+        vBox.addWidget(self.sleep_time_check_gBox)
+        vBox.addWidget(self.count_of_request)
+        vBox.addWidget(self.count_of_request_gBox)
+        optionsGBox.setLayout(vBox)
+        self.vBox.addWidget(optionsGBox)
+
+    def timeCheckHandler(self,checkBox):
+        if (checkBox.isChecked()):
+            self.sleep_time_check_gBox.setVisible(True)
+        else:
+            self.sleep_time_check_gBox.setVisible(False)
+
+    def countOfRequestCheckHandler(self,checkBox):
+        if (checkBox.isChecked()):
+            self.count_of_request_gBox.setVisible(True)
+        else:
+            self.count_of_request_gBox.setVisible(False)
+
+    def fileDialog(self):
+        self.fileGBox=QGroupBox("List Of Range File")
+        fileHbox=QHBoxLayout()
+        self.fileButton=QPushButton("Select File")
+        self.fileButton.clicked.connect(self.pushButton_handler)
+        fileHbox.addWidget(self.fileButton)
+        self.fileGBox.setLayout(fileHbox)
+
+    def rangeDialog(self):
+        self.rangeGBox = QGroupBox("Range (e.g. 1.1.1.1/24)")
+        rangeeHbox = QHBoxLayout()
+        self.range_edit = QLineEdit()
+        rangeeHbox.addWidget(self.range_edit)
+        self.rangeGBox.setLayout(rangeeHbox)
+
+    def open_dialog_box(self):
+        filedesc = QFileDialog.getOpenFileName()
+        path = filedesc[0]
+        fileName=path.split("/")
+        self.fileButton.setText(fileName[-1])
+
+
+    def pushButton_handler(self):
+        print("Button pressed")
+        self.open_dialog_box()
+
+    def comboBoxChange(self):
+        if (self.range_comboBox.currentText()=="Select One"):
+            self.comboBoxGbox.setVisible(False)
+        elif(self.range_comboBox.currentText()=="Range"):
+            self.comboBoxGbox.setVisible(True)
+            self.fileGBox.setVisible(False)
+            self.rangeGBox.setVisible(True)
+        elif(self.range_comboBox.currentText()=="List Of Range File"):
+            self.comboBoxGbox.setVisible(True)
+            self.rangeGBox.setVisible(False)
+            self.fileGBox.setVisible(True)
 
     def __del__(self):
         self.win.close()
--- runner.py
+++ runner.py
@@ -5,7 +5,7 @@ from PyQt5.QtWidgets import QApplication
 from UI import *
 
 #windows=Home()
-windows=Maskprocessor()
+windows=Netdiscover()
 def window():
     app = QApplication(sys.argv)
     windows.createWindow()
